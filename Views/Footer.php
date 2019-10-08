7f0f5cc6162b691b814a9a9a3bc8a844eaa1808a
Oscar Lista <43946678+olista94@users.noreply.github.com>
2019-10-08 19:34:13 +0200

    Pie de pagina

--- Views/Footer.php
+++ Views/Footer.php
@@ -5,9 +5,9 @@ Fecha: 20/12/2018-->
 	 <!--Pie-->
 <div class="footer" id="footer">
 	<!--Creador-->
-	<li><a href="../Views/Integrantes.php" target=_blank><?php echo $strings['Creado por']; ?>: Los Cangrejas </a></li>
+	<li>© TopPadel</li>
 	 <!--Fecha-->
-		<li><a><?php echo $strings['Fecha']; ?>: 21/12/2018</a></li>
+		<!--<li><a><?php echo $strings['Fecha']; ?>: 21/12/2018</a></li>-->
 </div>
 </body>
 </html>
