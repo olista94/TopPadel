e670c15946628c36953cf678d496ac5a1d33766a
Oscar Lista <43946678+olista94@users.noreply.github.com>
2019-10-16 12:17:40 +0200

    Add files via upload

---
+++ Reservas_ADD_Hora_View.php
@@ -0,0 +1,75 @@
+<!-- FORMULARIO PARA AÑADIR UNA FASE A UNA TAREA
+CREADO POR: Los Cangrejas
+Fecha: 26/12/2018-->
+
+<?php
+ //Declaracion de la clase 
+class Reservas_ADD_Hora{
+
+
+	//Descripcion de la tarea a la que pertenece la fase a añadir
+	var $horasLibres;
+	var $fecha_reserva;
+	
+	//Variable con el enlace al form de ADD fase
+	var $enlace;	
+	//Constructor de la clase
+	function __construct($horasLibres,$fecha_reserva,$enlace){
+				
+		$this -> horasLibres = $horasLibres;
+		$this -> fecha_reserva = $fecha_reserva;
+	
+		$this -> enlace = $enlace;		
+		$this -> mostrar();
+	}
+	//Funcion que "muestra" el contenido de la página
+	function mostrar(){
+		//Variable de idioma
+		if(!isset($_SESSION['idioma'])){
+            $_SESSION['idioma'] = 'SPANISH';
+        }
+		//Archivo del idioma
+        include '../Locales/Strings_'. $_SESSION['idioma'] .'.php';
+?>	 
+<!--Formulario para añadir fase-->
+		<div class="form">
+
+			<form name="registerForm" id="registerForm" method="post" action="../Controllers/Reservas_Controller.php" enctype="multipart/form-data">
+				<input type = "hidden" name = "fecha_reserva" value = '<?php echo $this->fecha_reserva; ?>'>
+					<input type = "hidden" name = "pista_ID_Pista" value = '<?php echo $this->pista; ?>'>
+				<legend><?php echo $strings['Añadir reserva'];?>
+				
+				</legend>
+
+				<div>
+				<!--Campo descripcion de la fase-->
+					
+				
+				<label>
+					<?php echo $strings['Hora']; ?></label>
+					<select name="hora_inicio">
+						<?php
+							while($horasLibres=$this->horasLibres->fetch_array()){
+
+						?>
+								<option value="<?php echo $horasLibres[0];?>"><?php echo $horasLibres[0];?>
+
+								</option>
+						<?php
+							}
+						?>
+					</select>
+
+				</div>
+				
+				<!--Boton para finalizar-->
+				<button type="submit" name="action" value="Confirmar_ADD_Hora" value="Submit" class="aceptar"></button>
+				<!--Boton de borrado de texto-->
+				<button type="reset" value="Reset" class="cancelar"></button>
+
+			</form> 
+		</div> 
+<?php
+	}
+}
+?>
